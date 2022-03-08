8884ebe8f22e6a67a339a81f47371855370947a8
Ethan Yu <captainethan138@gmail.com>
2022-03-08 01:46:54 -0500

    initial commit

---
+++ src/components/TransactionButton.tsx
@@ -0,0 +1,4 @@
+import { useSendTransaction } from "@usedapp/core"
+import { Button, Box, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+const { sendTransaction, state } = useSendTransaction()
---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+export const SocialMediaLinks = {
+    twitter: "https://www.twitter.com",
+    discord: "https://www.discord.gg",
+    opensea: "https://www.opensea.io",
+  };
+  
